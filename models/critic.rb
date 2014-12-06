b15cd81a6359cda3879bcc1ede661e12ec41d2c4
Danny Rosenblatt <dgrosenblatt@gmail.com>
2014-12-06 14:40:23 -0500

    Handle no search results, add critic scoring and number options

--- models/critic.rb
+++ models/critic.rb
@@ -1,4 +1,15 @@
 class Critic < ActiveRecord::Base
-  def agree_pct
+  def pct
+    ((agree.to_f / (agree + disagree)) * 100).to_i
+  end
+
+  def meter_color
+    if pct > 75
+      "alert"
+    elsif pct > 50
+      ""
+    else
+      "success"
+    end
   end
 end
--- server.rb
+++ server.rb
@@ -16,46 +16,72 @@ configure do
   set :token, ENV['TOKEN']
 end
 
-def movie_title(movie)
+def movie_rt_query(movie)
   query = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{settings.token}&q=#{URI::encode(movie)}"
-  movie_result = JSON.parse(Net::HTTP.get(URI(query)), symbolize_names:true)[:movies][0]
-  movie_result[:title]
-  # inspect the links that api call returns for something interesting
-  #handle nothing returned from api somehow
+  result = JSON.parse(Net::HTTP.get(URI(query)), symbolize_names:true)[:movies][0]
+  result
+  # handle when nothing is returned from api
+end
+
+def reviews_rt_query(id)
+  uri = "http://api.rottentomatoes.com/api/public/v1.0/movies/#{id}/reviews.json?review_type=top_critic&page_limit=50&page=1&apikey=#{settings.token}"
+  reviews = JSON.parse(Net::HTTP.get(URI(uri)), symbolize_names:true)[:reviews]
+end
+
+def fix_poster(link)
+  "#{link[0..-8]}det.jpg"
+end
+
+def process_reviews(reviews, opinion)
+  reviews.each do |review|
+    critic = Critic.find_by(name: review[:critic])
+    if opinion == review[:freshness] # share same opinion/agree
+      if critic
+        critic.update(agree: critic.agree+1)
+      else
+        Critic.create({ name: review[:critic], publication: review[:publication], agree: 1 })
+      end
+    else # disagree
+      if critic
+        critic.update(agree: critic.disagree+1)
+      else
+        Critic.create({ name: review[:critic], publication: review[:publication], disagree: 1 })
+      end
+    end
+  end
+end
+
+def imdb_link(rt_movie)
+  "http://www.imdb.com/title/tt#{rt_movie[:alternate_ids][:imdb]}"
 end
 
 
 get '/' do
-  @critics = Critic.all
+  @show = params[:show]
+  if @show == "all"
+    @critics = Critic.all.order('agree DESC')
+  else
+    @critics = Critic.all.order('agree DESC').limit(5)
+  end
   erb :home
 end
 
 get '/rate' do
+  @movie = movie_rt_query(params[:movie])
+  redirect '/' unless @movie
+  @poster_link = fix_poster(@movie[:posters][:detailed])
 
-  @movie = movie_title(params[:movie])
-  # get movie poster?
-
-  # upsert all the critics who reviewed the movie
-
-  # http://guides.rubyonrails.org/active_record_querying.html#scopes
-
-  # store movie name into a table??
-
-  erb :rate # update critics based on clicked "rotten" or "fresh"
+  erb :rate
 end
 
+post '/rate' do
+  @opinion = params[:opinion]
+  @reviews = reviews_rt_query(params[:movie_id])
+  process_reviews(@reviews, @opinion)
+  redirect '/'
+end
 
-# reviews = JSON.parse(Net::HTTP.get(URI("http://api.rottentomatoes.com/api/public/v1.0/movies/#{movie[:id]}/reviews.json?review_type=top_critic&page_limit=50&page=1&apikey=#{api_key}")), symbolize_names:true)[:reviews]
-# puts "Was \"#{movie[:title]}\" rotten or fresh?"
-# freshness = gets.chomp.downcase
-# reviews.each do |review|
-#   if !critic_profiles.has_key?("#{review[:critic]}, #{review[:publication]}")
-#     critic_profiles["#{review[:critic]}, #{review[:publication]}"] = { agree: 0, disagree: 0}
-#   end
-#
-#   if review[:freshness] == freshness
-#     critic_profiles["#{review[:critic]}, #{review[:publication]}"][:agree] += 1
-#   else
-#     critic_profiles["#{review[:critic]}, #{review[:publication]}"][:disagree] += 1
-#   end
-# end
+post '/reset' do
+  Critic.destroy_all if params[:reset] == "true"
+  redirect '/'
+end
